262f4b513f1bf7ecd1d92098c1a4b1ef3486abee
janhavi-ghanghav <janhavighanghav@gmail.com>
2026-03-25 00:36:19 +0530

    feat(auth): add user role management and authentication middleware

--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,5 +1,8 @@
 import express from 'express';
-import {loginUser} from "../controllers/auth.js";
+import {addUserRole,loginUser} from "../controllers/auth.js";
+import {isAuth} from "../middlewares/isAuth.js"
 const router = express.Router();
+
 router.post("/login", loginUser);
+router.put("/add/role", isAuth, addUserRole);
 export default router;
