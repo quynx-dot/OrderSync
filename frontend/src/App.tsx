b82387fd24d714cb58898b87e6f845b3c938953f
janhavi-ghanghav <janhavighanghav@gmail.com>
2026-03-27 13:13:44 +0530

    feat(Navbar): add Navbar component with cart and authentication links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,13 @@ import{Toaster} from'react-hot-toast';
 import ProtectedRoute from  './components/protectedRoute';
 import PublicRoute from './components/publicRoutes';
 import SelectRole from "./pages/SelectRole";
+import Navbar from "./components/navbar";
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Navbar/>
     <Routes>
       <Route element={<PublicRoute/>}>
         <Route path="/login" element={<Login/>}/>
