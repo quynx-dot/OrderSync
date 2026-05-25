1ad0cb067de3f7eeab70fbab05a3d2fe6d7b0506
janhavi-ghanghav <janhavighanghav@gmail.com>
2026-05-26 00:20:09 +0530

    feat(auth): add rate limiting to authentication route

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -3,13 +3,24 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoute from './routes/auth.js';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth",authRoute);
+const authLimiter= rateLimit({
+  windowMs:15*60*1000,
+  max:10,
+  standardHeaders:true,
+  legacyHeaders:false,
+  message:{message:"Too many login attempts, please try again later."},
+
+});
+
+app.use("/api/auth", authLimiter, authRoute);
 const PORT = process.env.PORT || 5000;
 connectDB();
 app.listen(PORT, () => {
